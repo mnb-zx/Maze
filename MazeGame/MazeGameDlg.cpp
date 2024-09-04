0f94f8ad9151d89b0feec1e6a3bf05a948d1017e
mnb-zx <13333463502@163.com>
2024-09-04 15:13:04 +0800

    Update MazeGameDlg.cpp
    delete

--- MazeGame/MazeGameDlg.cpp
+++ MazeGame/MazeGameDlg.cpp
@@ -100,30 +100,7 @@ void CMazeGameDlg::OnSysCommand(UINT nID, LPARAM lParam)
 //  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
 //  这将由框架自动完成。
 
-//void CMazeGameDlg::OnPaint()
-//{
-//	if (IsIconic())
-//	{
-//		CPaintDC dc(this); // 用于绘制的设备上下文
-//
-//		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
-//
-//		// 使图标在工作区矩形中居中
-//		int cxIcon = GetSystemMetrics(SM_CXICON);
-//		int cyIcon = GetSystemMetrics(SM_CYICON);
-//		CRect rect;
-//		GetClientRect(&rect);
-//		int x = (rect.Width() - cxIcon + 1) / 2;
-//		int y = (rect.Height() - cyIcon + 1) / 2;
-//
-//		// 绘制图标
-//		dc.DrawIcon(x, y, m_hIcon);
-//	}
-//	else
-//	{
-//		CDialogEx::OnPaint();
-//	}
-//}
+
 
 //当用户拖动最小化窗口时系统调用此函数取得光标
 //显示。
